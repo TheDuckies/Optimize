599c270cc80d0bee58d4035493d6da4fdbfc1db2
Devon Bain <Devon.Bain@gmail.com>
2012-06-20 14:25:34 -0400

    1.41 perf update

---
+++ ConversionOptimizer/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace ConversionOptimizer
+{
+    class Program
+    {
+        private static List<Test> TestList;
+        private static List<string> exceptionList;
+        private static Hashtable ExistingList;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(@"C:\Projects\Optimize\");
+
+            OpenFileDialog ReadFile = FindFitNesseList();
+
+            BuildExceptionList();
+            
+            ReadSource(new StreamReader(ReadFile.OpenFile()));
+
+        }
+
+        public static OpenFileDialog FindFitNesseList()
+        {
+            OpenFileDialog ReadFile = new OpenFileDialog();
+            ReadFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            ReadFile.InitialDirectory = @"C:\Projects\Optimize\FitNesseList\";
+            ReadFile.Title = "Select a tab-delimited source file";
+
+            DialogResult result = ReadFile.ShowDialog();
+
+            while (result != DialogResult.OK)
+            {
+                if (result == DialogResult.Cancel || result == DialogResult.Abort)
+                   Environment.Exit(0);
+                if (!ReadFile.CheckFileExists)
+                    continue;
+                result = ReadFile.ShowDialog();
+            }
+
+            return ReadFile;
+        }
+
+        public static void BuildExceptionList()
+        {
+            OpenFileDialog exceptionFile = new OpenFileDialog();
+            exceptionFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            exceptionFile.InitialDirectory = @"C:\Projects\Optimize\ExceptionsList\";
+            exceptionFile.Title = "Select an exception list";
+            TextReader exceptionListFile;
+            DialogResult result2 = exceptionFile.ShowDialog();
+
+            if (result2 == DialogResult.Cancel || result2 == DialogResult.Abort)
+            {
+                exceptionListFile = new StreamReader(@"C:\Projects\Optimize\ExceptionsList\blank.txt");
+            }
+            else
+            {
+                exceptionListFile = new StreamReader(exceptionFile.OpenFile());
+            }
+
+            exceptionList = new List<string>();
+
+            while (exceptionListFile.Peek() != -1)
+                exceptionList.Add(exceptionListFile.ReadLine());
+        }
+
+        /*
+         * TODO
+         * 
+         */
+        public static void ReadSource(StreamReader source)
+        {
+
+            StreamWriter outputFile = new StreamWriter("OUTPUT.TXT");
+            StreamWriter spreadsheetoutput = new StreamWriter("Spreadsheet.TSV");
+            spreadsheetoutput.WriteLine("Path" + '\t' + "Lines" + '\t' + "Status");
+            TestList = new List<Test>();
+
+            using (TextReader reader = source)
+            {
+                string line;
+                int count = 0;
+                while( (line = reader.ReadLine()) != null)
+                {
+                    char[] toRemove = {'\t'};
+                    
+                    string[] testline = line.Split(toRemove, StringSplitOptions.RemoveEmptyEntries);
+
+                    Test newTest = new Test(testline[0], exceptionList);
+
+                    if(newTest.Status == null)
+                        newTest.Status = testline[1];
+                    newTest.FitnessePath = testline[0];
+                    TestList.Add(newTest);
+
+                    count++;
+                }
+                outputFile.WriteLine("Total number of Tests: " + count);
+
+                try
+                {
+
+                    TestList.Sort();
+                }
+                catch (Exception e)
+                {
+                }
+
+                foreach (Test test in TestList)
+                {
+                   outputFile.WriteLine("Test Path: " + test.FullPath + '\t' + "Line Count: " + test.NumLines + '\t' + "Status: " + test.Status);
+                   spreadsheetoutput.WriteLine(test.FitnessePath + '\t' + test.NumLines + '\t' + test.Status);
+                }
+                outputFile.Close();
+                spreadsheetoutput.Close();
+            }
+        }
+
+    }
+}
---
+++ ConversionOptimizer/Test.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConversionOptimizer
+{
+    public class Test : IComparable<Test>
+    {
+        public bool Convertable;
+        public string FitnessePath;
+        public string FullPath;
+        public int NumLines;
+        public string Status;
+
+        public Test(string path, List<string> exceptions)
+        {
+            //Get Full Path and add context.txt
+            FullPath = @"C:\Projects\FitNesseRoot\" + path.Replace('.', '\\') + @"\content.txt";
+
+            //Count number of lines
+            NumLines = 0;
+
+            TextReader testFile;
+
+            try
+            {
+                testFile = new StreamReader(FullPath);
+
+               while(testFile.Peek() != -1)
+               {
+                   NumLines++;
+
+                   string currLine = testFile.ReadLine().ToUpper();
+
+                   if (exceptions != null)
+                       foreach (string ex in exceptions)
+                       {
+                           if (currLine.Contains(ex))
+                           {
+                               Convertable = false;
+
+                               if(Status != null && Status.Contains("On Hold:"))
+                               {
+                                   Status = Status + " " + ex;
+                               }
+                               else
+                               {
+                                   Status = "On Hold: " + ex;
+                               }
+                           }
+                       }
+               }
+            }
+            catch (FileNotFoundException e)
+            {
+                NumLines = -1;
+                Status = "Test Not Found";
+                Convertable = false;
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                NumLines = -1;
+                Status = "Test Directory Not Found";
+                Convertable = false;
+            }
+
+
+            if (NumLines == -1)
+                return;
+
+            //Search for unconvertable syntax
+
+        }
+
+        #region IComparable<Test> Members
+
+        public int CompareTo(Test other)
+        {
+            if (statusCompare().CompareTo(other.statusCompare()) == 0)
+                return NumLines.CompareTo(other.NumLines);
+            else
+            {
+                return statusCompare().CompareTo(other.statusCompare());
+            }
+        }
+
+        
+
+        public int statusCompare()
+        {
+            switch (Status)
+            {
+                case "Not Started":
+                    return 0;
+                case "In Progress":
+                    return 1;
+                case "On Hold":
+                    return 2;
+                case "Waiting for Review":
+                    return 4;
+                case "Finished":
+                    return 5;
+                default:
+                    return 3;
+            }
+        }
+
+        #endregion
+    }
+}
